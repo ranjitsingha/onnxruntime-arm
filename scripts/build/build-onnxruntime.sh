4d827c0da4bd1730452b42ea29d15987a51be8e2
Mike Roberts <2947595+m-roberts@users.noreply.github.com>
2021-04-21 13:31:40 +0100

    Use steps that work on RPi

--- scripts/build/build-onnxruntime.sh
+++ scripts/build/build-onnxruntime.sh
@@ -21,16 +21,23 @@ fi
 # Docs
 # https://www.onnxruntime.ai/docs/how-to/build.html
 
-# Start the basic build
-./build.sh --use_openmp --parallel --config ${BUILDTYPE} ${ARCH_ARGS} --update --build
+# Currently working command
+./build.sh --use_openmp --config "${BUILDTYPE}" ${ARCH_ARGS} --update --build --build_shared_lib --build_wheel
 
-# Build Shared Library
-./build.sh --use_openmp --parallel --config ${BUILDTYPE} ${ARCH_ARGS} --build_shared_lib
+########################
+# SEPARATE THE BUILDS? #
+########################
 
-# Build Python Bindings and Wheel
-./build.sh --use_openmp --parallel --config ${BUILDTYPE} ${ARCH_ARGS} --enable_pybind --build_wheel
+# # Start the basic build
+# ./build.sh --use_openmp --parallel --config ${BUILDTYPE} ${ARCH_ARGS} --update --build
 
-# Build NuGet package
-./build.sh --use_openmp --parallel --config ${BUILDTYPE} ${ARCH_ARGS} --build_nuget
+# # Build Shared Library
+# ./build.sh --use_openmp --parallel --config ${BUILDTYPE} ${ARCH_ARGS} --build_shared_lib
+
+# # Build Python Bindings and Wheel
+# ./build.sh --use_openmp --parallel --config ${BUILDTYPE} ${ARCH_ARGS} --enable_pybind --build_wheel
+
+# # Build NuGet package
+# ./build.sh --use_openmp --parallel --config ${BUILDTYPE} ${ARCH_ARGS} --build_nuget
 
 mv build/* /build/
--- scripts/setup/install-prereqs.sh
+++ scripts/setup/install-prereqs.sh
@@ -9,30 +9,26 @@ IFS=$'\n\t'
 
 apt-get update
 
-apt-get install -t buster-backports -y \
+apt-get -t buster-backports install -y \
     build-essential \
-    cmake \
     curl \
     libcurl4-openssl-dev \
     libssl-dev \
     wget \
     python3 \
-    python3-dev \
-    python3-numpy \
     python3-pip \
-    python3-setuptools \
-    python3-wheel \
+    python3-dev \
     git \
     tar \
     libatlas-base-dev \
-    libopenblas-dev \
-    mono-complete \
-    mono-devel
+    cmake \
+    protobuf-compiler
+# libopenblas-dev
+# mono-complete
+# mono-devel
 
 # git workaround: https://superuser.com/questions/1642858/git-throws-fatal-unable-to-access-https-github-com-user-repo-git-failed-se
 apt-get install -y --allow-downgrades libcurl3-gnutls=7.64.0-4+deb10u2
 
 # Install latest versions
-python3 -m pip install --upgrade pip setuptools wheel
-python3 -m pip install numpy
-python3 -m pip install flake8
+python3 -m pip install --upgrade pip setuptools wheel numpy flake8
